f0babd817baa29a7801a90306df60e8f819c7520
YJ Soon <yjsoon@tinkertanker.com>
2024-03-06 02:40:04 +0000

    Added image generation

--- pages/0_Animation_Demo.py
+++ pages/0_Animation_Demo.py
@@ -1,84 +1,43 @@
-# Copyright (c) Streamlit Inc. (2018-2022) Snowflake Inc. (2022)
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from typing import Any
-
-import numpy as np
-
 import streamlit as st
-from streamlit.hello.utils import show_code
+import openai
+
+def generate_image(prompt): 
+    client = openai.OpenAI(api_key=st.secrets["openai_api_key"])
+    response = client.images.generate(
+        prompt=prompt,
+        n=1,
+        size="256x256"
+    )  
+    image_url = response.data[0].url
+    return image_url
+
+def image_gen() -> None:
+    st.set_page_config(
+        page_title="Image Generation", 
+        page_icon="📸")
+    st.markdown("# Create Your Animal")
+    st.sidebar.header("Image Generation")
+
+    # Split page into 2 columns
+    col1, col2 = st.columns ([3, 5])
+
+    # Set up the options
+    image_style = col2.selectbox('Choose your image style',["A photo of a ","An oil painting of a "])
+    animal = col2.selectbox('Choose your animal',["baby panda ","British shorthair cat ","deer","Shiba Inu dog ","raccoon"])
+    activity = col2.selectbox('Choose your activity',["playing a guitar ","skateboarding ", "riding a bike", "studying for exams"])
+    input_text = st.text_area("Any additional image styling requests")
+    run_button = st.button("Run")
+
+    prompt = f"Help me generate an image based on the \
+        following: {image_style} {animal} {activity}, with \
+        some additional information: {input_text}. \
+        Make sure it's very amusing."
+
+    if run_button and input_text.strip() != "": 
+        with col1: 
+            with st.spinner("Loading"):
+                image_url = generate_image(prompt)
+                st.image(image_url)
 
 
-def animation_demo() -> None:
-
-    # Interactive Streamlit elements, like these sliders, return their value.
-    # This gives you an extremely simple interaction model.
-    iterations = st.sidebar.slider("Level of detail", 2, 20, 10, 1)
-    separation = st.sidebar.slider("Separation", 0.7, 2.0, 0.7885)
-
-    # Non-interactive elements return a placeholder to their location
-    # in the app. Here we're storing progress_bar to update it later.
-    progress_bar = st.sidebar.progress(0)
-
-    # These two elements will be filled in later, so we create a placeholder
-    # for them using st.empty()
-    frame_text = st.sidebar.empty()
-    image = st.empty()
-
-    m, n, s = 960, 640, 400
-    x = np.linspace(-m / s, m / s, num=m).reshape((1, m))
-    y = np.linspace(-n / s, n / s, num=n).reshape((n, 1))
-
-    for frame_num, a in enumerate(np.linspace(0.0, 4 * np.pi, 100)):
-        # Here were setting value for these two elements.
-        progress_bar.progress(frame_num)
-        frame_text.text("Frame %i/100" % (frame_num + 1))
-
-        # Performing some fractal wizardry.
-        c = separation * np.exp(1j * a)
-        Z = np.tile(x, (n, 1)) + 1j * np.tile(y, (1, m))
-        C = np.full((n, m), c)
-        M: Any = np.full((n, m), True, dtype=bool)
-        N = np.zeros((n, m))
-
-        for i in range(iterations):
-            Z[M] = Z[M] * Z[M] + C[M]
-            M[np.abs(Z) > 2] = False
-            N[M] = i
-
-        # Update the image placeholder by calling the image() function on it.
-        image.image(1.0 - (N / N.max()), use_column_width=True)
-
-    # We clear elements by calling empty on them.
-    progress_bar.empty()
-    frame_text.empty()
-
-    # Streamlit widgets automatically run the script from top to bottom. Since
-    # this button is not connected to any other logic, it just causes a plain
-    # rerun.
-    st.button("Re-run")
-
-
-st.set_page_config(page_title="Animation Demo", page_icon="📹")
-st.markdown("# Animation Demo")
-st.sidebar.header("Animation Demo")
-st.write(
-    """This app shows how you can use Streamlit to build cool animations.
-It displays an animated fractal based on the the Julia Set. Use the slider
-to tune different parameters."""
-)
-
-animation_demo()
-
-show_code(animation_demo)
+image_gen()
